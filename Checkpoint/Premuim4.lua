67d63d15bb9eb5fb519f059bc7be6c1e525fa3cf
glyphaj <glyphaj10@outlook.com>
2025-05-11 09:01:21 +0100

    Create Premuim4.lua

---
+++ Checkpoint/Premuim4.lua
@@ -0,0 +1,220 @@
+-- Checkpoint 4.
+local function _0x1a2b3c()
+    local _0x12bca4 = math.random(1, 100)
+    local _0x2f3a56 = math.floor(math.sqrt(_0x12bca4))
+    local _0x48bc9d = _0x12bca4 + _0x2f3a56
+    if _0x48bc9d % 2 == 0 then
+        return true
+    else
+        return false
+    end
+end
+
+local function _0x3d4e5f()
+    local _0x7f8a9b = string.sub("hello world", 1, 5)
+    local _0x23ab56 = string.byte(_0x7f8a9b, 1)
+    local _0x876abc = _0x23ab56 * 2
+    if _0x876abc > 150 then
+        return _0x7f8a9b
+    end
+end
+
+local function _0x4e5f6a()
+    local _0x432abc = math.random(1, 50)
+    local _0x5a6b7c = math.sqrt(_0x432abc)
+    if _0x5a6b7c > 5 then
+        return true
+    else
+        return false
+    end
+end
+
+local function _0x5f6a7b()
+    local _0x1f2b3c = string.upper("randomstring")
+    local _0x6d5e7f = string.find(_0x1f2b3c, "R")
+    if _0x6d5e7f then
+        return "Found!"
+    else
+        return "Not Found!"
+    end
+end
+
+local function _0x6a7b8c()
+    local _0x8d9eac = math.random(100, 1000)
+    local _0x234567 = _0x8d9eac / 10
+    return _0x234567
+end
+
+local function _0x7b8c9d()
+    local _0x1234ab = math.random(1, 20)
+    local _0x5678cd = math.pow(_0x1234ab, 2)
+    local _0x9876ef = math.sqrt(_0x5678cd)
+    if _0x9876ef == _0x1234ab then
+        return true
+    else
+        return false
+    end
+end
+
+local function _0x8c9d0a()
+    local _0xabcdef = string.char(97, 98, 99, 100)
+    local _0x543210 = string.sub(_0xabcdef, 2, 4)
+    local _0x3456ab = string.len(_0x543210)
+    return _0x3456ab
+end
+
+local function _0x9d0a1b()
+    local _0xabcdef = string.char(121, 122, 120)
+    local _0x234567 = string.reverse(_0xabcdef)
+    return _0x234567
+end
+
+local function _0xa1b2c3()
+    local _0x456789 = math.random(1, 100)
+    local _0x987654 = _0x456789 * math.random(1, 10)
+    return _0x987654
+end
+
+local function _0xb2c3d4()
+    local _0xabcdef = string.char(101, 102, 103)
+    local _0x54321ab = string.upper(_0xabcdef)
+    return _0x54321ab
+end
+
+local function _0xc3d4e5()
+    local _0x1234ab = math.random(50, 150)
+    local _0x5678cd = math.floor(math.sqrt(_0x1234ab))
+    return _0x5678cd
+end
+
+local function _0xd4e5f6()
+    local _0xabc123 = string.len("test")
+    local _0xdef456 = string.byte("test", 1)
+    if _0xabc123 == 4 then
+        return _0xdef456
+    end
+end
+
+local function _0xe5f6a7()
+    local _0x56789a = math.sin(math.pi / 4)
+    if _0x56789a > 0.5 then
+        return true
+    end
+end
+
+local function _0xf6a7b8()
+    local _0x234567 = string.sub("example", 2, 4)
+    local _0x567890 = math.sqrt(25)
+    if _0x567890 == 5 then
+        return _0x234567
+    end
+end
+
+local function _0xa7b8c9()
+    local _0x987654 = math.random(1, 1000)
+    local _0x1a2b3c = math.floor(math.sqrt(_0x987654))
+    return _0x1a2b3c
+end
+
+local function _0xb8c9d0()
+    local _0x1234ab = string.char(110, 111, 112, 113)
+    local _0x5678cd = string.upper(_0x1234ab)
+    if _0x5678cd == "NOPQ" then
+        return true
+    end
+end
+
+local function _0xc9d0e1()
+    local _0xabcdef = math.random(1, 100)
+    local _0x234567 = math.floor(math.sqrt(_0xabcdef))
+    return _0x234567
+end
+
+local function _0xd0e1f2()
+    local _0xabcdef = math.random(1, 500)
+    local _0x123456 = math.sqrt(_0xabcdef)
+    return _0x123456
+end
+
+local function _0xe1f2g3()
+    local _0xabcdef = string.reverse("test")
+    local _0x123456 = string.len(_0xabcdef)
+    return _0x123456
+end
+
+local function _0xf2g3h4()
+    local _0xabcdef = string.char(97, 98, 99)
+    local _0x234567 = string.byte(_0xabcdef, 2)
+    return _0x234567
+end
+
+local function _0xg3h4i5()
+    local _0xabcdef = string.find("hello world", "world")
+    return _0xabcdef
+end
+
+local function _0xh4i5j6()
+    local _0xabcdef = math.random(1, 200)
+    local _0x123456 = math.floor(math.sqrt(_0xabcdef))
+    return _0x123456
+end
+
+local function _0xi5j6k7()
+    local _0xabcdef = math.random(1, 10)
+    local _0x234567 = string.upper("sample")
+    return _0x234567
+end
+
+local function _0xj6k7l8()
+    local _0x234567 = math.random(1, 50)
+    local _0x876543 = string.len("example")
+    return _0x876543
+end
+
+local function _0xk7l8m9()
+    local _0xabcdef = math.random(1, 100)
+    local _0x234567 = math.pow(_0xabcdef, 2)
+    return _0x234567
+end
+
+local function _0xl8m9n0()
+    local _0xabcdef = string.char(120, 121, 122)
+    local _0x234567 = string.byte(_0xabcdef, 3)
+    return _0x234567
+end
+
+local function _0xm9n0o1()
+    local _0xabcdef = math.floor(math.random(10, 100))
+    local _0x234567 = string.sub("string", 2, 4)
+    return _0x234567
+end
+
+local function _0xn0o1p2()
+    local _0xabcdef = string.upper("hello")
+    local _0x234567 = math.random(10, 20)
+    return _0x234567
+end
+
+local function _0xo1p2q3()
+    local _0xabcdef = math.sqrt(144)
+    local _0x234567 = string.reverse("sample")
+    return _0x234567
+end
+
+local function _0xp2q3r4()
+    local _0xabcdef = math.floor(math.random(100, 1000))
+    local _0x234567 = math.sqrt(_0xabcdef)
+    return _0x234567
+end
+
+local function _0xq3r4s5()
+    local _0xabcdef = math.random(1, 200)
+    local _0x234567 = math.log(_0xabcdef)
+    return _0x234567
+end
+
+local function _0xr4s5t6()
+    local _0xabcdef = math.random(1, 1000)
+    local _0x234567 = math.floor(_0xabcdef / 2)
+    return _0x234567
+end
